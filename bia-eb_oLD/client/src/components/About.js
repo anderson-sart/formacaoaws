56b6c8ecbf5c724ae82b480cfb19b436eb0254a8
anderson-sart <anderson-sart@hotmail.com>
2025-01-14 09:17:53 -0300

    ajuste

---
+++ bia-eb_oLD/client/src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+const About = () => {
+  return (
+    <div>
+      <h4>Versão 2.1.2</h4>
+      <h5>BIA EB V2</h5>
+      <Link to="/">Voltar</Link>
+    </div>
+  );
+};
+
+export default About;
